e7a1d6925f2afb9f8cbcd88e4aa1ad48bb22c832
vaishnavnegi <vaishnavnegi207@gmail.com>
2023-06-30 14:54:47 +0200

    Refactor the NeuralNetwork class to add the regularization loss to the data loss.

--- src_to_implement/NeuralNetwork.py
+++ src_to_implement/NeuralNetwork.py
@@ -1,9 +1,7 @@
-## Implements the entire neural network skeleton ##
 import copy
 import numpy as np
 
 class NeuralNetwork:
-
     def __init__(self , optimizer , weights_initializer , bias_initializer):
         self.optimizer = optimizer
         self.loss = list()
@@ -26,38 +24,27 @@ class NeuralNetwork:
         self.__phase = val
 
     def forward(self):
-
         self.inp , self.label = self.data_layer.next()
-
         x = self.inp.copy()
-
         for layer in self.layers:
             layer.testing_phase = self.phase
             x = layer.forward(x)
-
         data_loss = self.loss_layer.forward(x , self.label)
-
         reg_loss = 0
         if self.optimizer.regularizer is not None:
             reg_loss = self.optimizer.regularizer.norm(data_loss)
-
         self.out = data_loss + reg_loss
-
         return self.out
 
     def backward(self):
-
         loss_grad = self.loss_layer.backward(self.label)
-
         for layer in self.layers[::-1]:
             loss_grad = layer.backward(loss_grad)
 
     def append_layer(self , layer):
-
         if layer.trainable == True:
             layer.optimizer = copy.deepcopy(self.optimizer)
             layer.initialize(self.weights_initializer, self.bias_initializer)
-
         self.layers.append(layer)
 
     def train(self , iterations):
@@ -73,7 +60,6 @@ class NeuralNetwork:
         for layer in self.layers:
             layer.testing_phase = self.phase
             x = layer.forward(x)
-
         return x
 
 
